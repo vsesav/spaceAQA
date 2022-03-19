9005ba9b1adb692ad71ad6c272ba8a97b7f9870b
v.savko <v.savko@space307.com>
2022-03-20 00:23:41 +0300

    Fixed selection of coordinates depending on the direction

--- src/main/java/lesson1/ex8/SeaBattle.java
+++ src/main/java/lesson1/ex8/SeaBattle.java
@@ -4,9 +4,11 @@ import java.util.Random;
 
 public class SeaBattle {
     private final String[][] ground = new String[10][10];
-    private static final String EMPTY_CELL = " • ";
-    private static final String ONE_DECK_OF_THE_SHIP = " ■ ";
+    private static final String EMPTY_CELL = "•";
+    private static final String ONE_DECK_OF_THE_SHIP = "■";
     private final Random random = new Random();
+    int x;
+    int y;
 
     public SeaBattle() {
         initialization(ground);
@@ -29,13 +31,18 @@ public class SeaBattle {
 
     // Создание поля морского боя
     private void initialization(String[][] array) {
-        for (int y = 0; y < array.length; y++) {
+        try {
+            for (int y = 0; y < array[0].length; y++) {
 
-            for (int x = 0; x < array.length; x++) {
+                for (int x = 0; x < array[0].length; x++) {
 
-                array[y][x] = EMPTY_CELL;
+                    array[y][x] = " " + EMPTY_CELL + " ";
 
+                }
             }
+        } catch (Exception indexOutOfBounds) {
+            System.out.println("Error: Array has no elements!");
+            System.exit(1);
         }
     }
 
@@ -43,7 +50,7 @@ public class SeaBattle {
     public void printGround() {
         for (String[] strings : ground) {
 
-            for (int i = 0; i < ground.length; i++) {
+            for (int i = 0; i < ground[0].length; i++) {
 
                 System.out.print(strings[i]);
             }
@@ -55,22 +62,19 @@ public class SeaBattle {
     // Рандомное добавление коробля заданного размера
     private void randomShipOfAGivenSize(int shipSize) {
         boolean direction = random.nextBoolean();
-
-        int x = random.nextInt(11 - shipSize);
-        int y = random.nextInt(11 - shipSize);
+        randomCoordinates(shipSize, direction);
 
         while (!verification(x, y, shipSize, direction)) {
-            x = random.nextInt(11 - shipSize);
-            y = random.nextInt(11 - shipSize);
+            randomCoordinates(shipSize, direction);
         }
 
         if (direction) {
             for (int shipY = y; shipY < y + shipSize; shipY++) {
-                ground[shipY][x] = ONE_DECK_OF_THE_SHIP;
+                ground[shipY][x] = " " + ONE_DECK_OF_THE_SHIP + " ";
             }
         } else {
             for (int shipX = x; shipX < x + shipSize; shipX++) {
-                ground[y][shipX] = ONE_DECK_OF_THE_SHIP;
+                ground[y][shipX] = " " + ONE_DECK_OF_THE_SHIP + " ";
             }
         }
     }
@@ -80,7 +84,7 @@ public class SeaBattle {
         int coordinate;
         int notBiggerThan;
         int a;
-        if (isItVertical){
+        if (isItVertical) {
             coordinate = y - 1;
             notBiggerThan = y + shipSize + 1;
             a = x;
@@ -94,10 +98,10 @@ public class SeaBattle {
             if (ship < 0 || ship > 9) {
                 continue;
             }
-            if ((a != 0 && checkContainShipDeck(ship, a - 1, isItVertical))
-                    || checkContainShipDeck(ship, a, isItVertical)
-                    || ((a < 9) && checkContainShipDeck(ship, a + 1, isItVertical))) {
-                return false;
+            for (int i = -1; i <= 1; i++) {
+                if (checkContainShipDeck(ship, a + i, isItVertical)) {
+                    return false;
+                }
             }
         }
         return true;
@@ -105,12 +109,26 @@ public class SeaBattle {
 
     //Проверка наличия палубы коробля в заданной координате
     private boolean checkContainShipDeck(int y, int x, boolean isItVertical) {
-        if (isItVertical) {
-            return ground[y][x].equals(ONE_DECK_OF_THE_SHIP);
-        } else {
-            return ground[x][y].equals(ONE_DECK_OF_THE_SHIP);
+        try {
+            if (isItVertical) {
+                return ground[y][x].contains(ONE_DECK_OF_THE_SHIP);
+            } else {
+                return ground[x][y].contains(ONE_DECK_OF_THE_SHIP);
+            }
+        } catch (Exception indexOutOfBounds) {
+            return false;
         }
+    }
 
+    //Рандомные координаты в зависимости от направления
+    private void randomCoordinates(int shipSize, boolean direction) {
+        if (direction) {
+            this.x = random.nextInt(10);
+            this.y = random.nextInt(11 - shipSize);
+        } else {
+            this.x = random.nextInt(11 - shipSize);
+            this.y = random.nextInt(10);
+        }
     }
 
 }
